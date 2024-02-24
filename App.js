9f3959e7dac33366378a31a9c9ac8608d64712cc
Mazen <Mazenadel19@gmail.com>
2024-02-24 04:19:07 +0200

    chore: :technologist: barebone react native project
    configured prettier, eslint, lintstage, husky, & commintlint

--- App.js
+++ App.js
@@ -1,62 +1,23 @@
-import { useState } from 'react'
-import { Button, FlatList, StyleSheet, View } from 'react-native'
-import GoalInput from './components/GoalInput'
-import GoalItem from './components/GoalItem'
-import { LIGHT_PURPLE } from './constants'
+import { StyleSheet, Text, View } from 'react-native'
+import { DARK_PURPLE, WHITE } from './constants'
 
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([])
-  const [isModalVisible, setIsModalVisible] = useState(false)
-
-  function openModal() {
-    setIsModalVisible(true)
-  }
-
-  function closeModal() {
-    setIsModalVisible(false)
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ])
-    closeModal()
-  }
-
-  function removeItem(id) {
-    setCourseGoals((list) => list.filter((item) => item.id !== id))
-  }
-
   return (
     <View style={styles.appContainer}>
-      <Button title="Add Goal" onPress={openModal} color={LIGHT_PURPLE} />
-      <GoalInput
-        addGoalHandler={addGoalHandler}
-        isModalVisible={isModalVisible}
-        onCancel={closeModal}
-      />
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => (
-            <GoalItem item={itemData.item} removeItem={removeItem} />
-          )}
-          keyExtractor={(item, index) => item.id}
-          alwaysBounceVertical={false}
-        />
-      </View>
+      <Text style={styles.welcome}>Hello World</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   appContainer: {
+    alignItems: 'center',
+    backgroundColor: DARK_PURPLE,
     flex: 1,
-    paddingHorizontal: 16,
-    paddingTop: 50,
+    justifyContent: 'center',
   },
-  goalsContainer: {
-    flex: 5,
+  welcome: {
+    color: WHITE,
+    fontSize: 26,
   },
 })
